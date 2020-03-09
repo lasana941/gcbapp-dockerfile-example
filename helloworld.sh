e507ea2bf2da9b4f6ded6203252fed4fb1978e6d
lasana941 <k.czechowicz941@gmail.com>
2020-03-09 17:03:15 +0100

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -13,4 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PORT="${PORT:-8080}"
+echo "Listening on ${PORT}..."
 echo "Hello, world!"
